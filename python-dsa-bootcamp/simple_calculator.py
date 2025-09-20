a6ad8df919da047c07a6e14d6607d8768db3263e
unknown <vysakh.thek@gmail.com>
2025-09-20 22:10:40 +0530

    simple calculator python syntax basics relearning

---
+++ python-dsa-bootcamp/simple_calculator.py
@@ -0,0 +1,13 @@
+num1 = float(input('Enter the first number'))
+num2 = float(input('Enter the second number'))
+
+sum = num1 + num2
+difference = num1 - num2
+multipication = num1*num2
+division = num1 / num2
+
+
+print("sum :", sum)
+print("difference :", difference)
+print("multipication :", multipication)
+print("division :", division)
