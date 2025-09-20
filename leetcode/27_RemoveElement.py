8769843fc736b193b8b8e7eee208ba97e9724b74
unknown <vysakh.thek@gmail.com>
2025-09-20 23:36:59 +0530

    leetcode remove element solved with pop and 2 pointer

---
+++ leetcode/27_RemoveElement.py
@@ -0,0 +1,23 @@
+
+## my initial appraoch using pop
+# complexity - O(n^2)
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+            else:
+                i += 1
+        return len(nums)
+    
+## optimal appraoch using two pointers
+# complexity - O(n)
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
