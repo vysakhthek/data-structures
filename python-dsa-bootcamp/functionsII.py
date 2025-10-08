5d642faa9ff36a40eac32a70bc5f07a138fd0cef
unknown <vysakh.thek@gmail.com>
2025-10-08 15:46:01 +0530

    Function lamda Added

--- python-dsa-bootcamp/functionsII.py
+++ python-dsa-bootcamp/functionsII.py
@@ -23,3 +23,42 @@ def is_valid_email(email: str) -> bool:
 
 print(is_valid_email("vysakh.thek@gmail.com"))
 print(is_valid_email("1@gmail.com"))
+
+
+even_or_odd = lambda num:num%2 ==0
+print(even_or_odd(6))
+
+
+nums = [1,2,3,4,5,6,7]
+
+squared = map(lambda num:num**2, nums)
+print(list(squared))
+
+
+nums2 = [1,2,3,4]
+nums1 = [4,5,6,7]
+
+added_nums = list(map(lambda num1,num2:num1+num2, nums1, nums2))
+print(added_nums)
+
+str_nums = ['1','2','3','4','5']
+int_nums = list(map(int, str_nums))
+
+print(int_nums)
+
+str_letters = ['a','b','c','d','e']
+upper_letters = list(map(str.upper, str_letters))
+
+print(upper_letters)
+
+
+def get_name(persons):
+    return persons['name']
+
+persons = [
+    {'name': 'vysakh'},
+    {'name': 'carpe'}
+]
+
+names = list(map(get_name, persons))
+print(names)
